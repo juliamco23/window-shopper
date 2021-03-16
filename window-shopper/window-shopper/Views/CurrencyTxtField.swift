cc2255826e37bef9ec648a197083e09fd9188e57
juliam <costajuliam@gmail.com>
2021-03-16 11:46:43 +0000

    new window shopper

--- window-shopper/window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -21,16 +21,15 @@ class CurrencyTxtField: UITextField {
     }
     
         func customizeView(){
-            backgroundColor = #colorLiteral(red: 0.9920735955, green: 0.9922121167, blue: 0.9920298457, alpha: 0.25)
+            backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2490035548)
             layer.cornerRadius = 5.0
             textAlignment = .center
             
             
             if let p = placeholder {
-        
-                let place = NSAttributedString(string: p, attributes: [ .foregroundColor: UIColor.white])
+                let place = NSAttributedString(string: p, attributes: [ .foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
         attributedPlaceholder = place
-            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         
     }
         
---
+++ window-shopper1/window-shopper1/Controller/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  window-shopper1
+//
+//  Created by Juliam Costa on 16/03/2021.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+
+
+}
+
